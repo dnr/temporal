1f0f2cc7470cf6e7df7bb32e13a6f8ef2b194906
David Reiss <david@temporal.io>
2025-06-17 00:25:32 -0700

    move code

---
+++ service/matching/fair_level.go
@@ -0,0 +1,46 @@
+package matching
+
+import (
+	"fmt"
+
+	persistencespb "go.temporal.io/server/api/persistence/v1"
+)
+
+type fairLevel struct {
+	pass int64
+	id   int64
+}
+
+func (l fairLevel) String() string {
+	return fmt.Sprintf("<%d,%d>", l.pass, l.id)
+}
+
+func fairLevelLess(a, b fairLevel) bool {
+	return a.pass < b.pass || a.pass == b.pass && a.id < b.id
+}
+
+func fairLevelComparator(aany, bany any) int {
+	a := aany.(fairLevel) // nolint:revive
+	b := bany.(fairLevel) // nolint:revive
+	if fairLevelLess(a, b) {
+		return -1
+	} else if fairLevelLess(b, a) {
+		return 1
+	}
+	return 0
+}
+
+func fairLevelMax(a, b fairLevel) fairLevel {
+	if fairLevelLess(a, b) {
+		return b
+	}
+	return a
+}
+
+func fairLevelPlusOne(a fairLevel) fairLevel {
+	return fairLevel{pass: a.pass, id: a.id + 1}
+}
+
+func allocatedTaskFairLevel(t *persistencespb.AllocatedTaskInfo) fairLevel {
+	return fairLevel{pass: t.PassNumber, id: t.TaskId}
+}
--- service/matching/fair_task_reader.go
+++ service/matching/fair_task_reader.go
@@ -3,7 +3,6 @@ package matching
 import (
 	"context"
 	"errors"
-	"fmt"
 	"slices"
 	"sync"
 	"sync/atomic"
@@ -26,12 +25,6 @@ import (
 )
 
 type (
-	// FIXME: can we just call this dbLevel and use it everywhere?!
-	fairLevel struct {
-		pass int64
-		id   int64
-	}
-
 	fairTaskReader struct {
 		backlogMgr *fairBacklogManagerImpl
 		subqueue   int
@@ -513,38 +506,4 @@ func (tr *fairTaskReader) doGC(ackLevel fairLevel) {
 	} else {
 		tr.numToGC = max(0, tr.numToGC-n)
 	}
-}
-
-func (l fairLevel) String() string {
-	return fmt.Sprintf("<%d,%d>", l.pass, l.id)
-}
-
-func fairLevelLess(a, b fairLevel) bool {
-	return a.pass < b.pass || a.pass == b.pass && a.id < b.id
-}
-
-func fairLevelComparator(aany, bany any) int {
-	a := aany.(fairLevel) // nolint:revive
-	b := bany.(fairLevel) // nolint:revive
-	if fairLevelLess(a, b) {
-		return -1
-	} else if fairLevelLess(b, a) {
-		return 1
-	}
-	return 0
-}
-
-func fairLevelMax(a, b fairLevel) fairLevel {
-	if fairLevelLess(a, b) {
-		return b
-	}
-	return a
-}
-
-func fairLevelPlusOne(a fairLevel) fairLevel {
-	return fairLevel{pass: a.pass, id: a.id + 1}
-}
-
-func allocatedTaskFairLevel(t *persistencespb.AllocatedTaskInfo) fairLevel {
-	return fairLevel{pass: t.PassNumber, id: t.TaskId}
 }
